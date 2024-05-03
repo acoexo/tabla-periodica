bc7ca1b6b7677c1335ee09e693ae6e83cf818d77
acoexo <acseoyo04@gmail.com>
2024-05-03 13:07:09 +0100

    v0.1

---
+++ index.php
@@ -0,0 +1,146 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./src/css/styles.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <main>
+        <section class="tabla">
+            <div class="Celda H">H </div>
+            <div class="Celda He">He</div>
+
+            <div class="Celda Li">Li</div>
+            <div class="Celda Be">Be</div>
+            <div class="Celda B">B </div>
+            <div class="Celda C">C </div>
+            <div class="Celda N">N </div>
+            <div class="Celda O">O </div>
+            <div class="Celda F">F </div>
+            <div class="Celda Ne">Ne</div>
+
+            <div class="Celda Na">Na</div>
+            <div class="Celda Mg">Mg </div>
+            <div class="Celda Al">Al</div>
+            <div class="Celda Si">Si</div>
+            <div class="Celda P">P </div>
+            <div class="Celda S">S </div>
+            <div class="Celda Cl">Cl</div>
+            <div class="Celda Ar">Ar</div>
+
+            <div class="Celda K">K </div>
+            <div class="Celda Ca">Ca</div>
+            <div class="Celda Sc">Sc</div>
+            <div class="Celda Ti">Ti</div>
+            <div class="Celda V">V </div>
+            <div class="Celda Cr">Cr</div>
+            <div class="Celda Mn">Mn</div>
+            <div class="Celda Fe">Fe</div>
+            <div class="Celda Co">Co</div>
+            <div class="Celda Ni">Ni</div>
+            <div class="Celda Cu">Cu</div>
+            <div class="Celda Zn">Zn</div>
+            <div class="Celda Ga">Ga</div>
+            <div class="Celda Ge">Ge</div>
+            <div class="Celda As">As</div>
+            <div class="Celda Se">Se</div>
+            <div class="Celda Br">Br</div>
+            <div class="Celda Kr">Kr</div>
+
+            <div class="Celda Rb">Rb</div>
+            <div class="Celda Sr">Sr</div>
+            <div class="Celda Y">Y </div>
+            <div class="Celda Zr">Zr</div>
+            <div class="Celda Nb">Nb</div>
+            <div class="Celda Mo">Mo</div>
+            <div class="Celda Tc">Tc</div>
+            <div class="Celda Ru">Ru</div>
+            <div class="Celda Rh">Rh</div>
+            <div class="Celda Pd">Pd</div>
+            <div class="Celda Ag">Ag</div>
+            <div class="Celda Cd">Cd</div>
+            <div class="Celda In">In</div>
+            <div class="Celda Sn">Sn</div>
+            <div class="Celda Sb">Sb</div>
+            <div class="Celda Te">Te</div>
+            <div class="Celda I">I </div>
+            <div class="Celda Xe">Xe</div>
+
+
+            <div class="Celda Cs">Cs</div>
+            <div class="Celda Ba">Ba</div>
+            <div class="Celda Lu">Lu</div>
+            <div class="Celda Hf">Hf</div>
+            <div class="Celda Ta">Ta</div>
+            <div class="Celda W">W </div>
+            <div class="Celda Re">Re</div>
+            <div class="Celda Os">Os</div>
+            <div class="Celda Ir">Ir</div>
+            <div class="Celda Pt">Pt</div>
+            <div class="Celda Au">Au</div>
+            <div class="Celda Hg">Hg</div>
+            <div class="Celda Tl">Tl</div>
+            <div class="Celda Pb">Pb</div>
+            <div class="Celda Bi">Bi</div>
+            <div class="Celda Po">Po</div>
+            <div class="Celda At">At</div>
+            <div class="Celda Rn">Rn</div>
+
+
+            <div class="Celda Fr">Fr</div>
+            <div class="Celda Ra">Ra</div>
+            <div class="Celda Lr">Lr</div>
+            <div class="Celda Rf">Rf</div>
+            <div class="Celda Db">Db</div>
+            <div class="Celda Sg">Sg</div>
+            <div class="Celda Bh">Bh</div>
+            <div class="Celda Hs">Hs</div>
+            <div class="Celda Mt">Mt</div>
+            <div class="Celda Ds">Ds</div>
+            <div class="Celda Rg">Rg</div>
+            <div class="Celda Cn">Cn</div>
+            <div class="Celda Nh">Nh</div>
+            <div class="Celda Fl">Fl</div>
+            <div class="Celda Mc">Mc</div>
+            <div class="Celda Lv">Lv</div>
+            <div class="Celda Ts">Ts</div>
+            <div class="Celda Og">Og</div>
+
+            <div class="Celda La">La</div>
+            <div class="Celda Ce">Ce</div>
+            <div class="Celda Pr">Pr</div>
+            <div class="Celda Nd">Nd</div>
+            <div class="Celda Pm">Pm</div>
+            <div class="Celda Sm">Sm</div>
+            <div class="Celda Eu">Eu</div>
+            <div class="Celda Gd">Gd</div>
+            <div class="Celda Tb">Tb</div>
+            <div class="Celda Dy">Dy</div>
+            <div class="Celda Ho">Ho</div>
+            <div class="Celda Er">Er</div>
+            <div class="Celda Tm">Tm</div>
+            <div class="Celda Yb">Yb</div>
+
+            <div class="Celda Ac">Ac</div>
+            <div class="Celda Th">Th</div>
+            <div class="Celda Pa">Pa</div>
+            <div class="Celda U">U </div>
+            <div class="Celda Np">Np</div>
+            <div class="Celda Pu">Pu</div>
+            <div class="Celda Am">Am</div>
+            <div class="Celda Cm">Cm</div>
+            <div class="Celda Bk">Bk</div>
+            <div class="Celda Cf">Cf</div>
+            <div class="Celda Es">Es</div>
+            <div class="Celda Fm">Fm</div>
+            <div class="Celda Md">Md</div>
+            <div class="Celda No">No</div>
+        </section>
+    </main>
+</body>
+
+</html>
